8756ce831ba3ca28f238a9669809deee4698f551
Fatou Dieye <137110529+dfabbie@users.noreply.github.com>
2025-05-06 16:01:54 -0700

    Create WrongTurnPin.swift

---
+++ Live Hike/Live Hike/WrongTurnPin.swift
@@ -0,0 +1,15 @@
+import Foundation
+import CoreLocation
+
+struct WrongTurnPin: Identifiable, Codable {
+    var id = UUID()
+    let description: String
+    let landmarks: String
+    let latitude: Double
+    let longitude: Double
+    let createdAt = Date()
+    
+    var coordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+}
