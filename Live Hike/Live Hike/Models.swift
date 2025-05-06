80b45f62eee92763d52cef46bbbb7f51f14807b5
Fatou Dieye <137110529+dfabbie@users.noreply.github.com>
2025-05-06 16:13:15 -0700

    Create Models.swift

---
+++ Live Hike/Live Hike/Models.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+struct Hazard: Identifiable, Codable {
+    let id = UUID()
+    let type: String
+    let severity: String
+    let description: String
+    let reportedDate: String  
+    let status: String
+    let trailName: String
+}
+
+struct WrongTurnPin: Identifiable, Codable {
+    var id = UUID()
+    let description: String
+    let landmarks: String
+    let latitude: Double
+    let longitude: Double
+    let createdAt = Date()
+}
