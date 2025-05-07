5c17db8859396d21405dbeaecba2104698b3553e
Fatou Dieye <137110529+dfabbie@users.noreply.github.com>
2025-05-06 21:53:49 -0700

    Update WrongTurnPin.swift to original

--- Live Hike/Live Hike/WrongTurnPin.swift
+++ Live Hike/Live Hike/WrongTurnPin.swift
@@ -3,14 +3,19 @@ import CoreLocation
 
 struct WrongTurnPin: Identifiable, Codable {
     var id = UUID()
-    let trailId: String
     let description: String
-    let landmarks: String 
+    let trailId: String
+    let landmarks: String
     let latitude: Double
     let longitude: Double
     let createdAt = Date()
     
     var coordinate: CLLocationCoordinate2D {
         CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+    
+    
+    private enum CodingKeys: String, CodingKey {
+        case id, description, trailId, landmarks, latitude, longitude, createdAt
     }
 }
