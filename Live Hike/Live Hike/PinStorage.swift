4445e849b211e07f79e32619991f243a44211fc6
Fatou Dieye <137110529+dfabbie@users.noreply.github.com>
2025-05-06 21:20:48 -0700

    Update PinStorage.swift to original

--- Live Hike/Live Hike/PinStorage.swift
+++ Live Hike/Live Hike/PinStorage.swift
@@ -19,26 +19,9 @@ class PinStorage {
         return []
     }
     
-    func loadPinsForTrail(trailId: String) -> [WrongTurnPin] {
-        let allPins = loadPins()
-        return allPins.filter { $0.trailId == trailId }
-    }
-    
     func addPin(_ pin: WrongTurnPin) {
         var pins = loadPins()
         pins.append(pin)
         savePins(pins)
     }
-    
-    func deletePin(withId id: UUID) {
-        var pins = loadPins()
-        pins.removeAll { $0.id == id }
-        savePins(pins)
-    }
-
-    func getUserPins() -> [WrongTurnPin] {
-        let allPins = loadPins()
-        return allPins.filter { $0.userId == UserManager.shared.userId }
-    }
 }
-
