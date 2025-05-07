bcf718a1ebd52928243fe33e944774879103f10b
Fatou Dieye <137110529+dfabbie@users.noreply.github.com>
2025-05-06 21:49:43 -0700

    Update HazardStorage.swift

--- Live Hike/Live Hike/HazardStorage.swift
+++ Live Hike/Live Hike/HazardStorage.swift
@@ -1,44 +1,44 @@
-import Foundation
+// import Foundation
 
-class HazardStorage {
-    static let shared = HazardStorage()
+// class HazardStorage {
+//     static let shared = HazardStorage()
     
-    private let userHazardsKey = "userReportedHazards"
+//     private let userHazardsKey = "userReportedHazards"
     
-    func saveHazards(_ hazards: [Hazard]) {
-        if let encoded = try? JSONEncoder().encode(hazards) {
-            UserDefaults.standard.set(encoded, forKey: userHazardsKey)
-            UserDefaults.standard.synchronize()
-        }
-    }
+//     func saveHazards(_ hazards: [Hazard]) {
+//         if let encoded = try? JSONEncoder().encode(hazards) {
+//             UserDefaults.standard.set(encoded, forKey: userHazardsKey)
+//             UserDefaults.standard.synchronize()
+//         }
+//     }
     
-    func loadHazards() -> [Hazard] {
-        if let data = UserDefaults.standard.data(forKey: userHazardsKey),
-           let hazards = try? JSONDecoder().decode([Hazard].self, from: data) {
-            return hazards
-        }
-        return []
-    }
+//     func loadHazards() -> [Hazard] {
+//         if let data = UserDefaults.standard.data(forKey: userHazardsKey),
+//            let hazards = try? JSONDecoder().decode([Hazard].self, from: data) {
+//             return hazards
+//         }
+//         return []
+//     }
     
-    func loadHazardsForTrail(trailName: String) -> [Hazard] {
-        let allHazards = loadHazards()
-        return allHazards.filter { $0.trailName == trailName }
-    }
+//     func loadHazardsForTrail(trailName: String) -> [Hazard] {
+//         let allHazards = loadHazards()
+//         return allHazards.filter { $0.trailName == trailName }
+//     }
     
-    func addHazard(_ hazard: Hazard) {
-        var hazards = loadHazards()
-        hazards.append(hazard)
-        saveHazards(hazards)
-    }
+//     func addHazard(_ hazard: Hazard) {
+//         var hazards = loadHazards()
+//         hazards.append(hazard)
+//         saveHazards(hazards)
+//     }
     
-    func deleteHazard(withId id: UUID) {
-        var hazards = loadHazards()
-        hazards.removeAll { $0.id == id }
-        saveHazards(hazards)
-    }
+//     func deleteHazard(withId id: UUID) {
+//         var hazards = loadHazards()
+//         hazards.removeAll { $0.id == id }
+//         saveHazards(hazards)
+//     }
 
-    func getUserHazards() -> [Hazard] {
-        let allHazards = loadHazards()
-        return allHazards.filter { $0.userId == UserManager.shared.userId }
-    }
-}
+//     func getUserHazards() -> [Hazard] {
+//         let allHazards = loadHazards()
+//         return allHazards.filter { $0.userId == UserManager.shared.userId }
+//     }
+// }
