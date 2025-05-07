26163926ba6564663a95a5553817058c181f0887
Fatou Dieye <137110529+dfabbie@users.noreply.github.com>
2025-05-06 20:34:16 -0700

    Update WrongTurnPin.swift

--- Live Hike/Live Hike/WrongTurnPin.swift
+++ Live Hike/Live Hike/WrongTurnPin.swift
@@ -2,14 +2,26 @@ import Foundation
 import CoreLocation
 
 struct WrongTurnPin: Identifiable, Codable {
-    var id = UUID()
+    var id: UUID
     let description: String
-     let trailId: String
+    let trailId: String
     let landmarks: String
     let latitude: Double
     let longitude: Double
     let createdAt = Date()
     let userId: String
+
+    init(id: UUID = UUID(), description: String, trailId: String, landmarks: String,
+         latitude: Double, longitude: Double, createdAt: Date = Date(), userId: String) {
+        self.id = id
+        self.description = description
+        self.trailId = trailId
+        self.landmarks = landmarks
+        self.latitude = latitude
+        self.longitude = longitude
+        self.createdAt = createdAt
+        self.userId = userId
+    }
     
     var coordinate: CLLocationCoordinate2D {
         CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
